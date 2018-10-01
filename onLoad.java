5b3a8d27861423fce8dbe3beaf05682c650e6d3a
smitteh1 <42484571+smitteh1@users.noreply.github.com>
2018-09-30 20:43:23 -0400

    Committed file OnLoad.java
    ++

---
+++ onLoad.java
@@ -0,0 +1,11 @@
+// In the package mainpkg.
+package mainpkg;
+
+// Opens Class onLoad
+public class onload {
+
+	// Main function run when open.
+	public static void main(String[] args) {
+		System.out.print("Created By James Ash (github.com/smitteh1)");
+	}
+}
